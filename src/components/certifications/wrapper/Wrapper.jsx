a6971afea1b56e217dae9b3bbb80d089fa820e00
Nikhil-Chinthala <nikky.chainthala2@gmail.com>
2025-05-21 23:53:15 +0530

    code new

--- src/components/certifications/wrapper/Wrapper.jsx
+++ src/components/certifications/wrapper/Wrapper.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Content from '../../content/Content';
 import './Wrapper.css';
+
+
 const Wrapper = () => {
   return (
     <div className='wrapperContainer content'>
       <Content />
+    
+
      
     </div>
   );
